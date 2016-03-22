46a349939c19874b0e1ed0e984ae79c91232612f
Norton Pengra <npengra317@gmail.com>
2016-03-22 14:09:39 -0700

    Wrote tests for smaller functions for update_vals

--- src/test_scraper.py
+++ src/test_scraper.py
@@ -1,4 +1,4 @@
-from scraper import get_inspection_page
+from scraper import update_vals, SCRAPE_VARS, get_inspection_page
 import pytest
 
 
@@ -10,5 +10,36 @@ def empty_request():
 
 @pytest.fixture(scope='function')
 def seattle_request():
-    """Create a request for every seattle restruant."""
+    """Create a request for every seattle restraunt."""
     return {"City": "Seattle"}
+
+
+def test_update_null(empty_request):
+    """Test that we create a blank search properly."""
+    result = update_vals(**empty_request)
+    expected = SCRAPE_VARS['PARAMS']
+    assert result == expected
+
+
+def test_update_seattle(seattle_request):
+    """Test that we create a proper seattle search."""
+    result = update_vals(**seattle_request)
+    expected = {
+        'Output': 'W',
+        'Business_Name': '',
+        'Business_Address': '',
+        'Longitude': '',
+        'Latitude': '',
+        'City': 'Seattle',
+        'Zip_Code': '',
+        'Inspection_Type': 'All',
+        'Inspection_Start': '',
+        'Inspection_End': '',
+        'Inspection_Closed_Business': 'A',
+        'Violation_Points': '',
+        'Violation_Red_Points': '',
+        'Violation_Descr': '',
+        'Fuzzy_Search': 'N',
+        'Sort': 'H'
+    }
+    assert result == expected
