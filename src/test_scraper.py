34252278d5b1344217b939a0b20c2d8d6ae459be
Norton Pengra <npengra317@gmail.com>
2016-03-22 13:59:01 -0700

    Created fixtures for basic requests

--- src/test_scraper.py
+++ src/test_scraper.py
@@ -1,1 +1,14 @@
 from scraper import get_inspection_page
+import pytest
+
+
+@pytest.fixture(scope='function')
+def empty_request():
+    """Create an empty request."""
+    return {}
+
+
+@pytest.fixture(scope='function')
+def seattle_request():
+    """Create a request for every seattle restruant."""
+    return {"City": "Seattle"}
