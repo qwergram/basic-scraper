339109e35d1357b44a45f6fe34d7974296ffe85c
Norton Pengra <npengra317@gmail.com>
2016-03-22 15:43:25 -0700

    Created get post generator

--- src/test_scraper.py
+++ src/test_scraper.py
@@ -2,6 +2,7 @@ from scraper import (
     update_vals,
     SCRAPE_VARS,
     get_inspection_page,
+    format_get_request,
     send_request,
 )
 import pytest
@@ -50,7 +51,22 @@ def test_update_seattle(seattle_request):
     assert result == expected
 
 
-def test_send_request_dummy():
-    response = send_request(SCRAPE_VARS['DOMAIN'] + SCRAPE_VARS['PATH'], {})
-    import pdb; pdb.set_trace()
+def test_seattle_to_get(seattle_request):
+    full = update_vals(**seattle_request)
+    get_req = format_get_request(full)
+    expected = ("Output=W&Business_Name=&Business_Address=&Longitude=&Latitud"
+                "e=&City=Seattle&Zip_Code=&Inspection_Type=All&"
+                "Inspection_Start=3/1/2016&Inspection_End=3/22/2016&"
+                "Inspection_Closed_Business=A&Violation_Points=&Violation_Red"
+                "_Points=&Violation_Descr=&Fuzzy_Search=N&Sort=H")
+
+    for part in expected.split("&"):
+        assert part in get_req
+    assert get_req.startswith('?')
+    assert not get_req.endswith('&')
+
+def test_send_request_dummy(seattle_request):
+    params = update_vals(**seattle_request)
+    # response = send_request(SCRAPE_VARS['DOMAIN'] + SCRAPE_VARS['PATH'], params)
+    # import pdb; pdb.set_trace()
     assert 1 == 1
