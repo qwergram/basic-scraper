f37c1589c0e24314d1c00f182da557e43a8209c1
Norton Pengra <npengra317@gmail.com>
2016-03-22 13:51:22 -0700

    Created get_inspection_page

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,10 @@
+# coding=utf-8
+"""Scrape foodsafety data from kingcounty."""
+import requests
+
 SCRAPE_VARS = {
-    "DOMAIN": 'http://info.kingcounty.gov',
-    "PATH": '/health/ehs/foodsafety/inspections/Results.aspx',
+    "DOMAIN": 'http://info.kingcounty.gov/',
+    "PATH": 'health/ehs/foodsafety/inspections/Results.aspx',
     "PARAMS": {
         'Output': 'W',
         'Business_Name': '',
@@ -20,3 +24,15 @@ SCRAPE_VARS = {
         'Sort': 'H'
     }
 }
+
+def get_inspection_page(**kwargs):
+    endpoint = SCRAPE_VARS['DOMAIN'] + SCRAPE_VARS['PATH']
+    params = SCRAPE_VARS['PARAMS'].copy()
+    for key, val in kwargs.items():
+        if key in SCRAPE_VARS['PARAMS']:
+            params[key] = val
+        else:
+            raise KeyError("Illegal key '{}':'{}' passed in".format(key, val))
+    resp = requests.get(endpoint, params=params)
+    resp.raise_for_status()
+    return resp.content, resp.encoding
